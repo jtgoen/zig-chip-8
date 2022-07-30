4415c3881e99a20cac256af2f478da5b39185569
jtgoen <5403345+jtgoen@users.noreply.github.com>
2022-07-30 11:54:06 -0600

    Formatting

--- src/main.zig
+++ src/main.zig
@@ -8,21 +8,15 @@ pub fn main() anyerror!void {
 
     const allocator = arena.allocator();
 
-    var interpreter = Chip8{
-        .memory = try allocator.create([4096]u8),
-        .V = try allocator.create([16]u8),
-        .stack = try allocator.create([16]u16),
-        .screen = try allocator.create([chip8.resolution]u8),
-        .keypad = try allocator.create([16]u8)
-    };
+    var interpreter = Chip8{ .memory = try allocator.create([4096]u8), .V = try allocator.create([16]u8), .stack = try allocator.create([16]u16), .screen = try allocator.create([chip8.resolution]u8), .keypad = try allocator.create([16]u8) };
 
     try interpreter.initialize();
 
     _ = try interpreter.load("programs/test_opcode.ch8");
 
-    std.debug.print("Interpeter Initialized and program loaded! \n{}\nMemory:\n{s}\n", .{interpreter, interpreter.memory});
+    std.debug.print("Interpeter Initialized and program loaded! \n{}\nMemory:\n{s}\n", .{ interpreter, interpreter.memory });
 
-    while(true) {
+    while (true) {
         _ = try interpreter.emulateCycle();
         break;
     }
