270d743bf3d2863c3c96559062fc6c17191376ca
jtgoen <5403345+jtgoen@users.noreply.github.com>
2022-07-29 23:08:41 -0600

    Add skeleton for opcode decoding

--- src/chip8.zig
+++ src/chip8.zig
@@ -26,7 +26,7 @@ const fontset = [_]u8{
     0xF0, 0x80, 0x80, 0x80, 0xF0, // C
     0xE0, 0x90, 0x90, 0x90, 0xE0, // D
     0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 };
 
 pub const resolution = 64 * 32;
@@ -38,21 +38,21 @@ pub const Chip8 = struct {
     // General purpose registers, labeled V0-VF
     V: *[16]u8,
 
-    // Index register, generally used for storing memory addresses, 
+    // Index register, generally used for storing memory addresses,
     //  so only lowest 12 bits are usually used
     I: u16 = 0,
 
-    // Special purpose register for timing delays, decremented at 
+    // Special purpose register for timing delays, decremented at
     // a rate of 60Hz when non-zero
     delay_timer: u8 = 0,
-    // Special purpose register for timing how long to play the 
+    // Special purpose register for timing how long to play the
     //  Chip-8's buzzer, decremented at a rate of 60Hz when non-zero
     sound_timer: u8 = 0,
 
     // Program counter. Used to store the currently executing address
     pc: u16 = pc_init,
 
-    // Stores addresses that the interpreter should return to when 
+    // Stores addresses that the interpreter should return to when
     //  finishing subroutines (allows for up to 16 nested subroutines)
     stack: *[16]u16,
     // Stack pointer, points to current position on the stack
@@ -61,13 +61,12 @@ pub const Chip8 = struct {
     // Current parsed opcode to be decoded and executed by the interpreter
     opcode: u16 = 0,
 
-    // The screen with which to render the state of the program, 
+    // The screen with which to render the state of the program,
     //  representing a screen constrained by the resolution constant
     screen: *[resolution]u8,
 
     keypad: *[16]u8,
 
-    
     eti_660_start: u16 = 0x600,
     is_eti_660: bool = false,
 
@@ -85,16 +84,16 @@ pub const Chip8 = struct {
             self.pc = pc_init;
         }
 
-        clear(u8, self.memory);
-        var fontset_slice = self.memory[0x050..0x0A0];
-        for(fontset) |font_byte, i| {
-            fontset_slice[i] = font_byte;
-        }
-
         clear(u16, self.stack);
         self.sp = 0;
 
         clear(u8, self.V);
+
+        clear(u8, self.memory);
+        var fontset_slice = self.memory[0x050..0x0A0];
+        for (fontset) |font_byte, i| {
+            fontset_slice[i] = font_byte;
+        }
 
         self.delay_timer = 0;
         self.sound_timer = 0;
@@ -108,7 +107,7 @@ pub const Chip8 = struct {
     }
 
     fn clear(comptime T: type, array_to_clear: []T) void {
-        for(array_to_clear) |_, i| {
+        for (array_to_clear) |_, i| {
             array_to_clear[i] = 0;
         }
     }
@@ -128,5 +127,157 @@ pub const Chip8 = struct {
     pub fn emulateCycle(self: *Chip8) !void {
         var opcode: u16 = @as(u16, self.memory[self.pc]) << 8 | @as(u16, self.memory[self.pc + 1]);
         std.log.info("Fetched Opcode: {x}", .{opcode});
+
+        return try decode(self, opcode);
+    }
+
+    fn decode(self: *Chip8, opcode: u16) !void {
+        switch (opcode & 0xF000) {
+            0x0000 => {
+                switch (opcode & 0x0F00) {
+                    0x0000 => {
+                        switch (opcode & 0x00FF) {
+                            0x00E0 => { // 00E0: Clears the screen.
+
+                            },
+                            0x00EE => { // 00EE: Returns from a subroutine.
+
+                            },
+                            else => { // 0NNN: Calls machine code routine (RCA 1802 for COSMAC VIP) at address NNN. Not necessary for most ROMs.
+
+                            },
+                        }
+                    },
+                    else => { // 0NNN: Calls machine code routine (RCA 1802 for COSMAC VIP) at address NNN. Not necessary for most ROMs.
+
+                    },
+                }
+            },
+            0x1000 => { // 1NNN: Jumps to address NNN.
+
+            },
+            0x2000 => { // 2NNN: Calls subroutine at NNN.
+
+            },
+            0x3000 => { // 3XNN: Skips the next instruction if VX equals NN.
+
+            },
+            0x4000 => { // 4XNN: Skips the next instruction if VX does not equal NN.
+
+            },
+            0x5000 => {
+                if (opcode & 0xF00F == 0x5000) { // 5XY0: Skips the next instruction if VX equals VY.
+
+                } else {
+                    unknownOpcode(self, opcode);
+                }
+            },
+            0x6000 => { // 6XNN: Sets VX to NN.
+
+            },
+            0x7000 => { // 7XNN: Adds NN to VX. (Carry flag is not changed);
+
+            },
+            0x8000 => {
+                switch (opcode & 0xF00F) {
+                    0x8000 => { // 8XY0: Sets VX to the value of VY.
+
+                    },
+                    0x8001 => { // 8XY1: Sets VX to VX or VY. (Bitwise OR operation);
+
+                    },
+                    0x8002 => { // 8XY2: Sets VX to VX and VY. (Bitwise AND operation);
+
+                    },
+                    0x8003 => { // 8XY3: Sets VX to VX xor VY.
+
+                    },
+                    0x8004 => { // 8XY4: Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there is not.
+
+                    },
+                    0x8005 => { // 8XY5: VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there is not.
+
+                    },
+                    0x8006 => { // 8XY6: Stores the least significant bit of VX in VF and then shifts VX to the right by 1.
+
+                    },
+                    0x8007 => { // 8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there is not.
+
+                    },
+                    0x800E => { // 8XYE: Stores the most significant bit of VX in VF and then shifts VX to the left by 1.
+
+                    },
+                    else => unknownOpcode(self, opcode),
+                }
+            },
+            0x9000 => {
+                if (opcode & 0xF00F == 0x9000) { // 9XY0: Skips the next instruction if VX does not equal VY.
+
+                } else {
+                    unknownOpcode(self, opcode);
+                }
+            },
+            0xA000 => { // ANNN: Sets I to the address NNN
+
+            },
+            0xB000 => { // BNNN: Jumps to the address NNN plus V0.
+
+            },
+            0xC000 => { // CXNN: Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
+
+            },
+            0xD000 => { // DXYN: Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels. Each row of 8 pixels is read as bit-coded starting from memory location I; I value does not change after the execution of this instruction. As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that does not happen
+
+            },
+            0xE000 => {
+                switch (opcode & 0xF0FF) {
+                    0xE09E => { // EX9E: Skips the next instruction if the key stored in VX is pressed.
+
+                    },
+                    0xE0A1 => { // EXA1: Skips the next instruction if the key stored in VX is not pressed.
+
+                    },
+                    else => unknownOpcode(self, opcode),
+                }
+            },
+            0xF000 => {
+                switch (opcode & 0xF0FF) {
+                    0xF007 => { // FX07: Sets VX to the value of the delay timer.
+
+                    },
+                    0xF00A => { // FX0A: A key press is awaited, and then stored in VX. (Blocking Operation. All instruction halted until next key event);
+
+                    },
+                    0xF015 => { // FX15: Sets the delay timer to VX.
+
+                    },
+                    0xF018 => { // FX18: Sets the sound timer to VX.
+
+                    },
+                    0xF01E => { // FX1E: Adds VX to I. VF is not affected.
+
+                    },
+                    0xF029 => { // FX29: Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
+
+                    },
+                    0xF033 => { // FX33: Stores the binary-coded decimal representation of VX, with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2. (In other words, take the decimal representation of VX, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.);
+
+                    },
+                    0xF055 => { // FX55: Stores from V0 to VX (including VX) in memory, starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.
+
+                    },
+                    0xF065 => { // FX65: Fills from V0 to VX (including VX) with values from memory, starting at address I. The offset from I is increased by 1 for each value read, but I itself is left unmodified.
+
+                    },
+                    else => unknownOpcode(self, opcode),
+                }
+            },
+            else => unknownOpcode(self, opcode),
+        }
+    }
+
+    fn unknownOpcode(self: *Chip8, opcode: u16) void {
+        std.log.warn("Encountered unknown opcode {x}. Skipping.", .{opcode});
+        self.pc += 2;
     }
 };
