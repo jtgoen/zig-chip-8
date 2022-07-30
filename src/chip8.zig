ffe407a633a4c1336dd53a2a1c41cf208a875452
jtgoen <5403345+jtgoen@users.noreply.github.com>
2022-07-29 21:47:25 -0600

    Add fontset, load on initialize

--- src/chip8.zig
+++ src/chip8.zig
@@ -10,6 +10,25 @@ const pc_init: u16 = 0x200;
 //  as they start the program counter at a different memory address
 const eti_660_pc_init: u16 = 0x600;
 
+const fontset = [_]u8{
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
 pub const resolution = 64 * 32;
 
 pub const Chip8 = struct {
@@ -66,17 +85,19 @@ pub const Chip8 = struct {
             self.pc = pc_init;
         }
 
-        self.delay_timer = 0;
-        self.sound_timer = 0;
+        clear(u8, self.memory);
+        var fontset_slice = self.memory[0x050..0x0A0];
+        for(fontset) |font_byte, i| {
+            fontset_slice[i] = font_byte;
+        }
 
         clear(u16, self.stack);
         self.sp = 0;
 
         clear(u8, self.V);
 
-        clear(u8, self.memory);
-
-        // Load fontset
+        self.delay_timer = 0;
+        self.sound_timer = 0;
     }
 
     pub fn load(self: *Chip8, file_path: []const u8) !usize {
@@ -86,19 +107,15 @@ pub const Chip8 = struct {
         return try open_file.reader().readAll(self.workspace());
     }
 
-    fn clear(comptime T: type, arrayToClear: []T) void {
-        for(arrayToClear) |_, i| {
-            arrayToClear[i] = 0;
+    fn clear(comptime T: type, array_to_clear: []T) void {
+        for(array_to_clear) |_, i| {
+            array_to_clear[i] = 0;
         }
     }
 
-    // fn interpreterReserve(self: *Chip8) []u8 {
-    //     return self.memory[0x000..0x1FF];
-    // }
-
-    // fn fontSet(self: *Chip8) []u8 {
-    //     return self.memory[0x050..0x0A0];
-    // }
+    pub fn fontSet(self: *Chip8) []const u8 {
+        return self.memory[0x050..0x0A0];
+    }
 
     fn workspace(self: *Chip8) []u8 {
         if (self.is_eti_660) {
@@ -110,6 +127,6 @@ pub const Chip8 = struct {
 
     pub fn emulateCycle(self: *Chip8) !void {
         var opcode: u16 = @as(u16, self.memory[self.pc]) << 8 | @as(u16, self.memory[self.pc + 1]);
-        std.log.info("Pulled Opcode: {x}", .{opcode});
+        std.log.info("Fetched Opcode: {x}", .{opcode});
     }
 };
