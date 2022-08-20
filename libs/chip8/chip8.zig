01b9d2901270bb95bccb249ff6af5446d0054876
jtgoen <5403345+jtgoen@users.noreply.github.com>
2022-08-20 12:30:26 -0600

    Migrate to using simpler thread-local secure RNG

--- libs/chip8/chip8.zig
+++ libs/chip8/chip8.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const rndgen = std.rand.DefaultPrng;
+const random = std.crypto.random;
 const fs = std.fs;
 const file = fs.File;
 const file_reader = file.Reader;
@@ -306,7 +306,7 @@ pub const Chip8 = struct {
                 self.pc = @as(u16, addr);
             },
             0xC000 => { // CXNN: Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
-                self.V[opcode & 0x0F00] = rndgen.init(0).random().int(u8) & @truncate(u8, opcode & 0x00FF);
+                self.V[opcode & 0x0F00] = random.int(u8) & @truncate(u8, opcode & 0x00FF);
             },
             0xD000 => { // DXYN: Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels. Each row of 8 pixels is read as bit-coded starting from memory location I; I value does not change after the execution of this instruction. As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that does not happen
 
