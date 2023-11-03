35b45c7694e83dd288020c6866ca85d38c7e979a
jtgoen <5403345+jtgoen@users.noreply.github.com>
2023-11-03 00:32:26 -0600

    Add adaptation of SDL2 Display implementation
    - Much code copied and modified from https://handling.website/archives/1977#basic-display
    - Now can load a simple GUI display for loaded applications with `./zig-chip-8 /path/to/file.ch8` after `zig build`ing

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,14 @@
 const std = @import("std");
 const chip8 = @import("chip8");
 const Chip8 = chip8.Chip8;
+const sdlDisplay = @import("sdlDisplay");
+const SdlDisplay = sdlDisplay.SdlDisplay;
 
 const ch8_ext = ".ch8";
+
+//TODO: Need to better encapsulate this FPS bound inside chip8 or display
+const fps: f32 = 60.0;
+const fps_interval = 1000.0 / fps;
 
 pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -10,11 +16,13 @@ pub fn main() anyerror!void {
 
     const allocator = arena.allocator();
 
+    var screen = try allocator.create([chip8.resolution]u32);
+
     var interpreter = Chip8{
         .memory = try allocator.create([4096]u8),
         .V = try allocator.create([16]u8),
         .stack = try allocator.create([16]u16),
-        .screen = try allocator.create([chip8.resolution]u32),
+        .screen = screen,
         .screen_2d = try allocator.create([chip8.height][]u32),
         .keypad = try allocator.create([16]u8),
     };
@@ -31,8 +39,19 @@ pub fn main() anyerror!void {
         var bytes_read = try interpreter.load(path);
         std.debug.print("Interpeter Initialized and program loaded! {} bytes read.\n", .{bytes_read});
 
-        while (true) {
-            try interpreter.emulateCycle();
+        var display = try SdlDisplay.create("CHIP-8", 800, 600, chip8.width, chip8.height);
+        defer display.free();
+
+        var previous_time = std.time.milliTimestamp();
+        var current_time = std.time.milliTimestamp();
+        while (display.open) {
+            display.input();
+
+            current_time = std.time.milliTimestamp();
+            if (@as(f32, @floatFromInt(current_time - previous_time)) > fps_interval) {
+                try interpreter.emulateCycle();
+                display.draw(interpreter.screen);
+            }
         }
     } else {
         std.debug.print("Program file path not provided. Program not loaded! Exiting...", .{});
@@ -44,6 +63,16 @@ pub fn main() anyerror!void {
 /// - Is null (we've reached the end)
 fn hasCh8ExtOrNull(arg: ?([]const u8)) bool {
     return if (arg) |arg_slice| std.mem.endsWith(u8, arg_slice, ch8_ext) else true;
+}
+
+fn printScreen(interpreter: Chip8) void {
+    var hBar = [_]u8{'_'} ** (chip8.width + 4);
+
+    std.log.info("{s}", .{hBar});
+    for (interpreter.screen_2d) |row| {
+        std.log.info("||{b}||", .{row});
+    }
+    std.log.info("{s}", .{hBar});
 }
 
 test "basic test" {
